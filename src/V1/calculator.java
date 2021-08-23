116bdaf2c073c0a15f1b5cf9bf831d641624e68d
Vinojan1999 <vinojan02abhimanyu@gmail.com>
2021-08-23 16:07:45 +0530

    V1-Input validation

--- src/V1/calculator.java
+++ src/V1/calculator.java
@@ -1,3 +1,5 @@
+package src.V1;
+
 import java.io.*;
 import java.util.*;
 
--- src/V1/hello_world.java
+++ src/V1/hello_world.java
@@ -1,3 +1,5 @@
+package src.V1;
+
 public class hello_world {
     public static void main(String[] args) {
         System.out.println("Hello World.!");
