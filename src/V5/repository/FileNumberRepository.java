331ef5b67ccfcd900db163a3afb32d880adcb3b6
Vinojan1999 <vinojan02abhimanyu@gmail.com>
2021-10-11 12:08:05 +0530

    V5 : Added repository files

---
+++ src/V5/repository/FileNumberRepository.java
@@ -0,0 +1,23 @@
+package src.V5.repository;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class FileNumberRepository implements NumberRepository {
+
+    public Double[] getNumbers() throws IOException {
+
+        //read the numbers text file
+        List<String> numbersStrs = Files.readAllLines(
+                Paths.get("/home/vinojan/Documents/GitHub/Git-clone/Calculator/numbers.txt")
+        );
+
+        double number1 = Double.parseDouble((numbersStrs.get(0)));
+        double number2 = Double.parseDouble((numbersStrs.get(1)));
+
+        return new Double[]{number1, number2};
+
+    }
+}
---
+++ src/V5/repository/NumberRepository.java
@@ -0,0 +1,7 @@
+package src.V5.repository;
+
+import java.io.IOException;
+
+public interface NumberRepository {
+    Double[] getNumbers() throws IOException;//will change this throws part in future
+}
